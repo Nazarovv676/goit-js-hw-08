1ca31b3999bd79a41b2fba5aa6f09b4c40d6e9d9
Ivan Nazarov <nazarovv676@gmail.com>
2025-05-11 15:21:36 +0300

    Add initial project files including .gitignore, styles, HTML structure, and gallery functionality

---
+++ js/gallery.js
@@ -0,0 +1,42 @@
+import { galleryItems } from "./gallery-items.js";
+
+const galleryContainer = document.querySelector(".image-gallery");
+
+const galleryMarkup = galleryItems.map(({ fullImage, thumbnail, caption }) => {
+  return `<li class="image-item">
+  <a class="image-link" href=${fullImage}>
+    <img
+      class="image-preview"
+      src=${thumbnail}
+      data-source=${fullImage}
+      alt=${caption}
+    />
+  </a>
+</li>`;
+});
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup.join(""));
+
+const onGalleryClick = (event) => {
+  event.preventDefault();
+
+  const clickedImage = event.target;
+  if (clickedImage.tagName === "IMG") {
+    const lightboxInstance = basicLightbox.create(
+      `<div><img class="lightbox-image" src=${clickedImage.getAttribute(
+        "data-source"
+      )} alt=${clickedImage.alt}/></div>`,
+      {
+        onShow: (instance) => {
+          instance.element().querySelector("div").onclick = (event) => {
+            event.preventDefault();
+            instance.close();
+          };
+        },
+      }
+    );
+    lightboxInstance.show();
+  }
+};
+
+galleryContainer.addEventListener("click", onGalleryClick);
